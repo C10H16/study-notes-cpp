f9e8993febbcb061adfc2c2ad978b21835a1d152
C10H16 <810465688@qq.com>
2019-02-22 21:36:19 +0800

    添加选择排序、插入排序、归并排序

--- 算法/sort.cpp
+++ 算法/sort.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 //普通的冒泡排序算法 
-void bubbleSort1(vector<int> &num)
+void bubbleSort1(std::vector<int> &num)
 {
 	int n = num.size();
 	int i = 0;
@@ -14,14 +12,14 @@ void bubbleSort1(vector<int> &num)
 		{
 			if (num[i-1]>num[i])
 			{
-				swap(num[i-1],num[i]);
+				std::swap(num[i-1],num[i]);
 			}
 		}
 	}
 }
 
 //改进的冒泡排序算法，不需要完成全部n-1次循环，当排序完成时自动退出 
-void bubbleSort2(vector<int> &num)
+void bubbleSort2(std::vector<int> &num)
 {
 	bool sorted = false;
 	int n = num.size();
@@ -32,7 +30,7 @@ void bubbleSort2(vector<int> &num)
 		{
 			if (num[i-1]>num[i])
 			{
-				swap(num[i-1], num[i]);
+				std::swap(num[i-1], num[i]);
 				sorted = false;
 			}
 		}
@@ -41,7 +39,7 @@ void bubbleSort2(vector<int> &num)
 }
 
 //快速排序算法 
-void quickSort(vector<int> &num, int low, int high)
+void quickSort(std::vector<int> &num, int low, int high)
 {
 	if(low>=high)
 	{
@@ -69,15 +67,135 @@ void quickSort(vector<int> &num, int low, int high)
 	quickSort(num, first+1, high); //递归对后半部分排序 
 }
 
+// 选择排序
+void selectionSort(std::vector<int> &num)
+{
+	int n = num.size();
+	int min = 0;
+	for (int i = 0; i < n - 1; ++i)
+	{
+		min = i;
+		for (int j = i + 1; j < n; ++j)
+		{
+			if (num[j]<num[min])
+				min = j;
+			
+		}
+		std::swap(num[i], num[min]);
+	}
+}
+
+// 插入排序
+void insertionSort(std::vector<int> &num)
+{
+	int n = num.size();
+	int j = 0,key = 0;
+	for(int i = 1; i < n; ++i)
+	{
+		j = i-1;
+		key = num[i];
+		while (j>=0 && num[j]>key)
+		{
+			num[j+1] = num[j];
+			--j;
+		}
+		num[j+1] = key;
+	}
+}
+ 
+// 二分查找插入排序
+int searchInsert(std::vector<int>& nums, int target, int low, int high) 
+{ 
+        int mid = (high - low)/2;
+        while (low != high)
+        {
+            if (target>nums[mid])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+            mid = low + (high - low)/2;
+        }
+        return low;
+}
+
+void insertionSort2(std::vector<int> &num)
+{
+	int n = num.size();
+	int j = 0, k = 0, key = 0;
+	for(int i = 1; i < n; ++i)
+	{
+		key = num[i];
+		j = searchInsert(num, key, 0, i);
+		for (k = i;k > j; --k)
+		{
+			num[k] = num[k-1];
+		}
+		num[j] = key;
+	}
+}
+
+// 归并排序 递归
+void merge(std::vector<int> &num, int low, int mid, int high)
+{
+	std::vector<int> leftSorted(num.begin()+low, num.begin()+mid+1);
+	std::vector<int> rightSorted(num.begin()+mid+1, num.begin()+high+1);
+	int l = 0, r = 0,i = low;
+	while (l<leftSorted.size()&&r<rightSorted.size())
+	{
+		if (leftSorted[l] < rightSorted[r])
+		{
+			num[i] = leftSorted[l];
+			++l;
+		}
+		else
+		{
+			num[i] = rightSorted[r];
+			++r;
+		}
+		++i;
+	}
+	if (l < leftSorted.size())
+	{
+		for(l; l < leftSorted.size(); ++l)
+		{
+			num[i] = leftSorted[l];
+			++i;
+		}
+	}
+	else
+	{
+		for(r; r< rightSorted.size(); ++r)
+		{
+			num[i] = rightSorted[r];
+			++i;
+		}
+	}
+}
+
+void mergeSort(std::vector<int> &num, int low, int high)
+{
+	if (low>=high)
+		return;
+	int mid = (low + high) / 2;
+	mergeSort(num, low, mid);
+	mergeSort(num, mid+1, high);
+	merge(num, low, mid, high);
+}
+ 
+
 int main()
 {
 	//test 
-	vector<int> num = {5,2,4,7,6,1,3};  
-	quickSort(num,0,num.size()-1);
+	std::vector<int> num = {5,2,4,7,6,1,3};  
+	mergeSort(num, 0, num.size()-1);
 	for (int i = 0; i < num.size(); ++i)
 	{
-		cout << num[i] << " ";
+		std::cout << num[i] << " ";
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return 0;
-} 
+}
